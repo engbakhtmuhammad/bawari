9301bdab30c3ed25561c003a14de309b9ebec6ee
engbakhtmuhammad <bakhtkhilji23@gmail.com>
2024-03-15 17:23:38 +0500

    pdf invoice generator app updated

--- lib/view/purchase/purchase.dart
+++ lib/view/purchase/purchase.dart
@@ -58,64 +58,64 @@ class _PurchaseScreenState extends State<PurchaseScreen> {
   ];
 
   final tableHeaders = [
-      'Description',
-      'Quantity',
-      'Unit Price',
-      'VAT',
-      'Total',
-    ];
+    'Description',
+    'Quantity',
+    'Unit Price',
+    'VAT',
+    'Total',
+  ];
 
-    final tableData = [
-      [
-        'Coffee',
-        '7',
-        '\$ 5',
-        '1 %',
-        '\$ 35',
-      ],
-      [
-        'Blue Berries',
-        '5',
-        '\$ 10',
-        '2 %',
-        '\$ 50',
-      ],
-      [
-        'Water',
-        '1',
-        '\$ 3',
-        '1.5 %',
-        '\$ 3',
-      ],
-      [
-        'Apple',
-        '6',
-        '\$ 8',
-        '2 %',
-        '\$ 48',
-      ],
-      [
-        'Lunch',
-        '3',
-        '\$ 90',
-        '12 %',
-        '\$ 270',
-      ],
-      [
-        'Drinks',
-        '2',
-        '\$ 15',
-        '0.5 %',
-        '\$ 30',
-      ],
-      [
-        'Lemon',
-        '4',
-        '\$ 7',
-        '0.5 %',
-        '\$ 28',
-      ],
-    ];
+  final tableData = [
+    [
+      'Coffee',
+      '7',
+      '\$ 5',
+      '1 %',
+      '\$ 35',
+    ],
+    [
+      'Blue Berries',
+      '5',
+      '\$ 10',
+      '2 %',
+      '\$ 50',
+    ],
+    [
+      'Water',
+      '1',
+      '\$ 3',
+      '1.5 %',
+      '\$ 3',
+    ],
+    [
+      'Apple',
+      '6',
+      '\$ 8',
+      '2 %',
+      '\$ 48',
+    ],
+    [
+      'Lunch',
+      '3',
+      '\$ 90',
+      '12 %',
+      '\$ 270',
+    ],
+    [
+      'Drinks',
+      '2',
+      '\$ 15',
+      '0.5 %',
+      '\$ 30',
+    ],
+    [
+      'Lemon',
+      '4',
+      '\$ 7',
+      '0.5 %',
+      '\$ 28',
+    ],
+  ];
 
   @override
   void initState() {
@@ -355,13 +355,16 @@ class _PurchaseScreenState extends State<PurchaseScreen> {
                                             child: GestureDetector(
                                               onTap: () async {
                                                 // generate pdf file
-                final pdfFile = await PdfInvoiceApi.generate(
-                  PdfColors.blue,
-                  pw.Font.courier(),tableHeaders, tableData
-                );
+                                                final pdfFile =
+                                                    await PdfInvoiceApi
+                                                        .generate(
+                                                            PdfColors.blue,
+                                                            pw.Font.courier(),
+                                                            tableHeaders,
+                                                            tableData);
 
-                // opening the pdf file
-                FileHandleApi.openFile(pdfFile);
+                                                // opening the pdf file
+                                                FileHandleApi.openFile(pdfFile);
                                                 // Get.to(InvoiceScreen());
                                               },
                                               child: Image.asset(
